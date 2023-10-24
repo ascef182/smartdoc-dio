9ef3b94a0f927a46e96ba8f08f8474fc736c798e
ascefpam <ascefpam@gmail.com>
2023-10-24 13:06:51 -0300

    first push commit

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const language = "JavaScript";
+
+const themes = [
+{
+    name: "Variáveis",
+},
+{
+    name:"Tipo de dados",
+},
+{
+    name:"Operadores",
+},
+{
+    name:"Estruturas condicionais",
+},
+{
+    name: "Arrays",
+},
+{
+    name:"Objetos",
+},
+{
+    name:"Estruturas de repetição", 
+},
+{
+    name:"High order functions", 
+},
+{
+    name:"Functions", 
+},
+{
+    name:"Escopo de variáveis", 
+},
+{
+    name:"Assícronismos", 
+},
+{
+    name:"Then/Try e Catch", 
+},
+];
+
+themes.forEach((theme) =>{
+    theme.message =`Escreva uma dacoumentação com exemplos de como funciona ${theme.name} em ${language}, seja o mais detalhado possível, 
+    separe os exemplos por tópicos e não esqueça de explicar o que cada linha faz. Retorne a resposta em uma estrutura HTML para ser mostrado em tela, 
+    adicione classes do tailwind css para estilizar o conteúdo. Crie um texto longo e completo.`;
+});
+
+export default themes; 
